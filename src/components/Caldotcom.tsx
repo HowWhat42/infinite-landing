fc02122808e3abb97922a1019c17a838659b0a00
Charles Lapierre <charles.lapierre@infinite-lab.com>
2025-06-03 12:48:42 +0200

    feat(Contact): integrate Caldotcom component for contact form functionality
    - Added @calcom/embed-react dependency to package.json
    - Replaced the existing contact form with Caldotcom component for improved user experience
    - Updated pnpm-lock.yaml to include new dependencies and their resolutions

---
+++ src/components/Caldotcom.tsx
@@ -0,0 +1,17 @@
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+export default function Caldotcom() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({"namespace":"30min"});
+      cal("ui", {"cssVarsPerTheme":{"light":{}, "dark":{"cal-brand":"#AE5DC7"}}, "hideEventTypeDetails":false,"layout":"month_view"});
+    })();
+  }, [])
+  return <Cal namespace="30min"
+    calLink="infinitelab/30min"
+    style={{width:"100%",height:"100%",overflow:"scroll"}}
+    config={{"layout":"month_view"}}
+    
+    
+  />;
+};
